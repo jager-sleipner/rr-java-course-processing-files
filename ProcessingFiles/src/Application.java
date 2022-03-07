1326dfb5600c2d38c158f9573e76c27b3190e6a2
metalmod <schaulin.martin@gmail.com>
2022-03-07 16:11:45 +0100

    Initial commit of project.

---
+++ ProcessingFiles/src/Application.java
@@ -0,0 +1,39 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Application {
+
+	public static void main(String[] args) {
+
+//		for (int i=0; i <3; i++) {
+//			Scanner input = new Scanner(System.in); 
+//			System.out.println("Enter some Text: ");
+//			String enteredText = input.nextLine();
+//			System.out.println(enteredText);
+//		}
+//		
+		try {
+			File file = new File("myfile.txt");
+			Scanner input = new Scanner(file);
+
+			while (input.hasNextLine()) {
+				String line = input.nextLine();
+				System.out.println(line);
+			}
+			input.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found");
+//			e.printStackTrace();
+		}
+	
+		MyFileUtils myUtil = new MyFileUtils();
+		try {
+			System.out.println(myUtil.subtract10FromLargerNumber(9));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+}
